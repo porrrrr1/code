9bbd0ca96fd390146e219f0541622805ab7bb294
porrrrr1 <porkrubpom@gmail.com>
2024-09-07 15:56:02 +0700

    commit-message

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-red-500 p-4 text-center text-white mt-auto"> {/* Add mt-auto */}
-      <p>&copy; 2024 porramet saehao</p>
+      <p>&copy; 2024 porramet saehao 67319010020</p>
     </footer>
   );
 };
